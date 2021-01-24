7f54c2e69dd4342104de3c09be62a72f8d07a02b
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2021-01-24 15:32:57 +0000

    Created a simple API
    - Endpoint `/generator/password/{length}`
    - Tests
    - GitHub actions

---
+++ src/LucasRosinelli.Pipeline.Api/Controllers/GeneratorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LucasRosinelli.Pipeline.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GeneratorController : ControllerBase
+    {
+        /// <summary>
+        /// Generate a password with the given length.
+        /// </summary>
+        /// <param name="length">The password length.</param>
+        /// <returns>The password.</returns>
+        [HttpGet("password/{length}")]
+        public IActionResult GeneratePassword(int length = 16)
+        {
+            if (length <= 0)
+            {
+                return BadRequest("The length must be greater than zero.");
+            }
+
+            const string validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&+-*.,@?=[]()";
+            var random = new Random();
+            var password = new StringBuilder();
+            while (password.Length < length)
+            {
+                password.Append(validCharacters[random.Next(0, validCharacters.Length)]);
+            }
+
+            return Ok(password.ToString());
+        }
+    }
+}
---
+++ tests/LucasRosinelli.Pipeline.Api.Tests/Controllers/GeneratorControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using LucasRosinelli.Pipeline.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace LucasRosinelli.Pipeline.Api.Tests.Controllers
+{
+    public class GeneratorControllerTests
+    {
+        private readonly GeneratorController _controller;
+
+        public GeneratorControllerTests()
+        {
+            _controller = new GeneratorController();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-50)]
+        public void GeneratePassword_WithInvalidLength_ShouldReturnBadRequest(int length)
+        {
+            // Arrange
+
+            // Act
+            IActionResult result = _controller.GeneratePassword(length);
+
+            // Assert
+            var objectResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("The length must be greater than zero.", objectResult.Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(50)]
+        [InlineData(500)]
+        [InlineData(8000)]
+        public void GeneratePassword_WithValidLength_ShouldSucceed(int length)
+        {
+            // Arrange
+            bool HasOnlyValidCharacters(string password)
+            {
+                const string validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&+-*.,@?=[]()";
+
+                foreach (var c in password)
+                {
+                    if (!validCharacters.Contains(c))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            // Act
+            IActionResult result = _controller.GeneratePassword(length);
+
+            // Assert
+            var objectResult = Assert.IsType<OkObjectResult>(result);
+            var generated = Assert.IsType<string>(objectResult.Value);
+            Assert.Equal(length, generated.Length);
+            Assert.True(HasOnlyValidCharacters(generated));
+        }
+    }
+}
